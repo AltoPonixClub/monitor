2dafe53c7a0bab1a7110a529d4c31f30b703aca4
Luca Manolache <luca1230@gmail.com>
2021-12-21 15:01:58 -0800

    Added constants to a header file

---
+++ unit/src/constants.h
@@ -0,0 +1,21 @@
+//
+// Created by luca on 12/21/21.
+//
+
+#ifndef MONITOR_CONSTANTS_H
+#define MONITOR_CONSTANTS_H
+
+namespace constants {
+    int cameraId = 4;
+    cv::Size imgSize = cv::Size(320, 240);
+    cv::Size displaySize = cv::Size(2 * 320, 2 * 240);
+    std::string calibPath = "/home/luca/code/monitor/unit/outputs.yml";
+    std::vector<std::vector<std::vector<float>>> rawArucoPts = {{{0, 0, 0}, {0, 4.6, 0}, {4.6, 4.6, 0}, {4.6, 0, 0}},
+                                                 {{16.1, 0, 0}, {16.1, 4.6, 0}, {20.7, 4.6, 0}, {20.7, 0, 0}},
+                                                 {{0, 15.1, 0}, {0, 19.7, 0}, {4.6, 19.7, 0}, {4.6, 15.1, 0}},
+                                                 {{16.1, 15.1, 0}, {16.1, 19.7, 0}, {20.7, 19.7, 0}, {20.7, 15.1, 0}}};
+    std::vector<cv::Point2f> target = {cv::Point2f(0, 0), cv::Point2f(19.8, 0), cv::Point2f(19.8, 20.8), cv::Point2f(0, 20.8)};
+    std::vector<int> arucoIds = {24, 42, 69, 48};
+}
+
+#endif //MONITOR_CONSTANTS_H
