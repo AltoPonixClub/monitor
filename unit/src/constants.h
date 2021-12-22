580f95f8b30588468c6e53d13e29446714b54465
aoberai <aoberai05@gmail.com>
2021-12-21 18:24:34 -0800

    added img viewer on pangolin window - has some issues

--- unit/src/constants.h
+++ unit/src/constants.h
@@ -6,15 +6,15 @@
 #define MONITOR_CONSTANTS_H
 
 namespace constants {
-    int cameraId = 0;
-    int imgWidth = 640;
-    int imgHeight = 480;
-    std::string calibPath = "/home/aoberai/programming/altoponix/monitor/unit/outputs.yml";
-    std::vector<std::vector<std::vector<float>>> rawArucoPts = {{{0, 0, 0}, {0, 4.6, 0}, {4.6, 4.6, 0}, {4.6, 0, 0}},
+    const int cameraId = 0;
+    const int imgWidth = 640;
+    const int imgHeight = 480;
+    const std::string calibPath = "/home/aoberai/programming/altoponix/monitor/unit/outputs.yml";
+    const std::vector<std::vector<std::vector<float>>> rawArucoPts = {{{0, 0, 0}, {0, 4.6, 0}, {4.6, 4.6, 0}, {4.6, 0, 0}},
                                                  {{16.1, 0, 0}, {16.1, 4.6, 0}, {20.7, 4.6, 0}, {20.7, 0, 0}},
                                                  {{0, 15.1, 0}, {0, 19.7, 0}, {4.6, 19.7, 0}, {4.6, 15.1, 0}},
                                                  {{16.1, 15.1, 0}, {16.1, 19.7, 0}, {20.7, 19.7, 0}, {20.7, 15.1, 0}}};
-    std::vector<int> arucoIds = {24, 42, 69, 48};
+    const std::vector<int> arucoIds = {24, 42, 69, 48};
 }
 
 #endif //MONITOR_CONSTANTS_H
--- unit/src/main.cpp
+++ unit/src/main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
+
 #include <pangolin/display/display.h>
 #include <pangolin/display/view.h>
 #include <pangolin/handler/handler.h>
 #include <pangolin/gl/gldraw.h>
+#include <pangolin/video/video_input.h>
+#include <pangolin/gl/gl.h>
+
 #include "utils.h"
 #include "constants.h"
 
 using namespace std;
-using namespace constants;
 
-int main( int /*argc*/, char** /*argv*/ ) {
-    cv::VideoCapture cap(cameraId);
+int main(int /*argc*/, char ** /*argv*/ ) {
+    cv::VideoCapture cap(constants::cameraId);
     if (!cap.isOpened()) {
         cout << "Broken Camera";
         return -1;
     }
     cap.set(cv::CAP_PROP_FPS, 60);
-    cv::Size displaySize = cv::Size(imgWidth, imgHeight);
+    cv::Size displaySize = cv::Size(constants::imgWidth, constants::imgHeight);
     cv::Ptr<cv::aruco::Dictionary> arucoDictionary = cv::aruco::Dictionary::get(cv::aruco::DICT_5X5_100);
     cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
-    cv::FileStorage calibFile(calibPath, cv::FileStorage::READ);
+    cv::FileStorage calibFile(constants::calibPath, cv::FileStorage::READ);
     cout << calibFile.isOpened() << endl;
     auto cameraMatrix = calibFile.operator[]("K").mat(); // I think incorrect
     auto distCoeffs = calibFile.operator[]("D").mat(); // I think incorrect
@@ -32,10 +35,10 @@ int main( int /*argc*/, char** /*argv*/ ) {
     vector<vector<cv::Point3f>> arucoPts; // convert to Point3f instead of vector for i j k
     vector<vector<cv::Point2f>> arucoPts2d;
 
-    for (const auto& i : rawArucoPts) {
+    for (const auto &i: constants::rawArucoPts) {
         vector<cv::Point3f> tmp;
         vector<cv::Point2f> tmp2d;
-        for (auto j : i) {
+        for (auto j: i) {
             tmp.push_back(cv::Point3f(j[0], j[1], j[2]));
             tmp2d.push_back(cv::Point2f(j[0], j[1]));
         }
@@ -43,62 +46,76 @@ int main( int /*argc*/, char** /*argv*/ ) {
         arucoPts2d.push_back(tmp2d);
     }
 
-    cv::Ptr<cv::aruco::Board> board = cv::aruco::Board::create(arucoPts, arucoDictionary, arucoIds);
+    cv::Ptr<cv::aruco::Board> board = cv::aruco::Board::create(arucoPts, arucoDictionary, constants::arucoIds);
     vector<vector<cv::Point2f>> detectedArucoCorners, rejectedArucoCorners;
     vector<int> detectedArucoIds;
 
     cv::Vec3d rvec, tvec;
 
-    pangolin::CreateWindowAndBind("Main",640*1.5,480*1.5);
+    pangolin::CreateWindowAndBind("Main", 640 * 1.5, 480 * 1.5);
     glEnable(GL_DEPTH_TEST);
 
     // Define Projection and initial ModelView matrix
     pangolin::OpenGlRenderState s_cam(
-            pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
-            pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
+            pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+            pangolin::ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin::AxisY)
     );
 
     // Create Interactive View in window
     pangolin::Handler3D handler(s_cam);
-    pangolin::View& d_cam = pangolin::CreateDisplay()
-            .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f / 480.0f)
             .SetHandler(&handler);
 
+    // This view will take up no more than a third of the windows width or height, and it
+    // will have a fixed aspect ratio to match the image that it will display. When fitting
+    // within the specified bounds, push to the top-left (as specified by SetLock).
+    pangolin::View &d_image = pangolin::Display("image")
+            .SetBounds(2 / 3.0f, 1.0f, 0, 1 / 3.0f, 640.0 / 480)
+            .SetLock(pangolin::LockLeft, pangolin::LockTop);
+
     cv::Mat frame, displayFrame, undistortedFrame;
+    unsigned char *pangoImageArray = new unsigned char[3 * constants::imgWidth * constants::imgHeight];
+    pangolin::GlTexture imageTexture(constants::imgWidth, constants::imgHeight, GL_RGB, false, 0, GL_RGB,
+                                     GL_UNSIGNED_BYTE);
     while (!pangolin::ShouldQuit()) {
         // Clear screen and activate view to render into
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         d_cam.Activate(s_cam);
 
         cap >> frame;
+//        cvtColor(frame, frame, cv::RGB2BGR);
         displayFrame = frame.clone();
         undistortedFrame = frame.clone();
 
-        cv::aruco::detectMarkers(frame, arucoDictionary, detectedArucoCorners, detectedArucoIds, parameters, rejectedArucoCorners);
-        cv::aruco::refineDetectedMarkers(frame, board, detectedArucoCorners, detectedArucoIds, rejectedArucoCorners, cameraMatrix, distCoeffs);
-        for (auto corner : detectedArucoCorners)  {
-            for (auto pt : corner) {
-                cv::circle(frame, pt, 10, cv::Scalar(255, 255, 0), cv::FILLED);
+        cv::aruco::detectMarkers(frame, arucoDictionary, detectedArucoCorners, detectedArucoIds, parameters,
+                                 rejectedArucoCorners);
+        cv::aruco::refineDetectedMarkers(frame, board, detectedArucoCorners, detectedArucoIds, rejectedArucoCorners,
+                                         cameraMatrix, distCoeffs);
+        for (auto corner: detectedArucoCorners) {
+            for (auto pt: corner) {
+                cv::circle(displayFrame, pt, 10, cv::Scalar(255, 255, 0), cv::FILLED);
             }
         }
         vector<pair<int, vector<cv::Point2f>>> tmp_corners;
         vector<cv::Point2f> tmp_corners_src, tmp_corners_dst;
         if (detectedArucoIds.size() == 4) {
-            cv::aruco::estimatePoseBoard(detectedArucoCorners, detectedArucoIds, board, cameraMatrix, distCoeffs, rvec, tvec);
+            cv::aruco::estimatePoseBoard(detectedArucoCorners, detectedArucoIds, board, cameraMatrix, distCoeffs, rvec,
+                                         tvec);
             for (int i = 0; i < detectedArucoIds.size(); i++) {
                 tmp_corners.push_back(make_pair(detectedArucoIds[i], detectedArucoCorners[i]));
             }
             sort(tmp_corners.begin(), tmp_corners.end(), Utils::pairSortComparator);
 
-            for (auto i : tmp_corners) {
-                for (auto c : i.second) {
+            for (auto i: tmp_corners) {
+                for (auto c: i.second) {
                     tmp_corners_dst.push_back(c);
                 }
             }
-            for (auto i : arucoPts2d) {
-                for (auto c : i) {
-                    c.x = c.x * (float) imgWidth / 20.7;
-                    c.y = c.y * (float) imgHeight / 19.7;
+            for (auto i: arucoPts2d) {
+                for (auto c: i) {
+                    c.x = c.x * (float) constants::imgWidth / 20.7;
+                    c.y = c.y * (float) constants::imgHeight / 19.7;
                     tmp_corners_src.push_back(c); // Multiplier to make bigger in final image
                 }
             }
@@ -110,7 +127,7 @@ int main( int /*argc*/, char** /*argv*/ ) {
             //TODO: is it bad to create objects constantly in loop instead of initializing outside?
             cv::Mat H = cv::findHomography(tmp_corners_src, tmp_corners_dst, cv::RANSAC, 5);
             // Apply perspective transformation to original image
-            cv::warpPerspective(frame, undistortedFrame, H.inv(), displaySize, cv::INTER_LINEAR);
+            cv::warpPerspective(displayFrame, undistortedFrame, H.inv(), displaySize, cv::INTER_LINEAR);
 
             tmp_corners.clear();
             tmp_corners_src.clear();
@@ -119,16 +136,25 @@ int main( int /*argc*/, char** /*argv*/ ) {
             // TODO: use rodrigues on rvec and tvec to turn into projection matrix
         }
 
-        cv::resize(displayFrame, displayFrame, displaySize);
-
-        // TODO: Display image on pangolin window
-//        cv::imshow("Raw", displayFrame);
-        cv::imshow("Undistorted", undistortedFrame);
-        if ((char) cv::waitKey(1) == 27)
-            break;
+//        cv::imshow("Undistorted", undistortedFrame);
+//        if ((char) cv::waitKey(1) == 27)
+//            break;
 
         // Render OpenGL Cube
         pangolin::glDrawColouredCube();
+
+        cv::vconcat(displayFrame, undistortedFrame, displayFrame);
+        cv::resize(displayFrame, displayFrame, displaySize);
+        // TODO: clean up how these images are stacked because very scuffed atm
+        //TODO: fix this offset color how to switch just green and blue
+        //TODO: Clean up dimensions between pangolin window, actual image, and display Image which is big time mess
+        for(int i = 3 * constants::imgWidth * constants::imgHeight - 1; i >= 0; i--) {
+            pangoImageArray[i] = (unsigned char)(displayFrame.data[3 * constants::imgWidth * constants::imgHeight-i+1]);
+        }
+        imageTexture.Upload(pangoImageArray, GL_BGR, GL_UNSIGNED_BYTE);
+        d_image.Activate();
+        glColor3f(1, 1, 1);
+        imageTexture.RenderToViewport();
         // Swap frames and Process Events
         pangolin::FinishFrame();
     }
