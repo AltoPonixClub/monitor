e418fa6cdb8975daaf27a756cc55af08c6a66976
tillvit <lucas.j.tan@gmail.com>
2022-01-03 17:46:38 -0800

    Relocate libraries, some uploader changes

--- unit/src/main/cpp/subsystems/uploader.cpp
+++ unit/src/main/cpp/subsystems/uploader.cpp
@@ -1,14 +1,17 @@
 #include <config/constants.h>
 #include <utils/utils.h>
+#include <spdlog/spdlog.h>
 #include <subsystems/uploader.h>
 #include "restclient-cpp/restclient.h"
 
-//Update values using:
-//state->monitorMeasurements[constants::general::Measurements::ATMOSPHERIC_TEMP] = 1;
-//Add a new value by doing:
-//1. Update kNumMeasurements in constants::general
-//2. Add the new measurement in constants::general::Measurements
-//3. Add the string name (the json key that is used) to kMeasurementNames in upload.h
+/*
+ * Update values using:
+ * state->monitorMeasurements[constants::general::Measurements::ATMOSPHERIC_TEMP] = 1;
+ * Add a new value by doing:
+ * 1. Update kNumMeasurements in constants::general
+ * 2. Add the new measurement in constants::general::Measurements
+ * 3. Add the string name (the json key that is used) to kMeasurementNames in upload.h
+ */
 
 Uploader::Uploader(State *state, Commands *commands, Outputs *outputs) {
     state->lastTimeS = Utils::getUnixTimestamp();
@@ -26,33 +29,33 @@ void Uploader::read(State *state) {
 }
 
 void Uploader::calculate(State *state, Commands *commands, Outputs *outputs) {
-    outputs->jsonString = "";
+    outputs->jsonMeasurementData = "";
 
     //Generate JSON String
     if (state->doUpload) {
-        outputs->jsonString = "{\"monitor_id\": \"" + constants::uploader::kMonitorId + "\",";
+        outputs->jsonMeasurementData = "{\"monitor_id\": \"" + constants::uploader::kMonitorId + "\",";
         int valuesUploaded = 0;
         for (int i = 0; i < constants::general::kNumMeasurements; i++) {
-            if (state->uploadValues[i]) {
-                outputs->jsonString += "\"" + constants::uploader::kMeasurementNames[i] + "\": " +
+            if (commands->uploadValues[i]) {
+                outputs->jsonMeasurementData += "\"" + constants::uploader::kMeasurementNames[i] + "\": " +
                                        std::to_string(state->monitorMeasurements[i]) + ",";
                 valuesUploaded++;
             }
         }
-        outputs->jsonString.pop_back();
-        outputs->jsonString += "}";
+        outputs->jsonMeasurementData.pop_back();
+        outputs->jsonMeasurementData += "}";
         //If we don't have anything to upload, abort
         if (valuesUploaded == 0)
-            outputs->jsonString = "";
+            outputs->jsonMeasurementData = "";
     }
 }
 
 void Uploader::write(Outputs *outputs) {
-    if (outputs->jsonString != "") {
-        std::cout << "Uploading " << outputs->jsonString << std::endl;
-        RestClient::Response r = RestClient::post(constants::uploader::kUploadUrl, "application/json", outputs->jsonString);
+    if (outputs->jsonMeasurementData != "") {
+        spdlog::info("Uploader: Uploading " + outputs->jsonMeasurementData);
+        RestClient::Response r = RestClient::post(constants::uploader::kUploadUrl, "application/json", outputs->jsonMeasurementData);
         if (r.code != 200)
-            std::cout << "Upload to database failed with code " << r.code << "!" << std::endl;
+            spdlog::error("Uploader: Upload to database failed with code " + std::to_string(r.code) + "!");
     }
 }
 
