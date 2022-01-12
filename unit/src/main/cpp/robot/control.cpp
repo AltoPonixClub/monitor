048b30fc1d34bac24196586f0f013a915b91c5ac
aoberai <aoberai05@gmail.com>
2022-01-12 12:52:18 -0800

    this should work

--- unit/src/main/cpp/robot/control.cpp
+++ unit/src/main/cpp/robot/control.cpp
@@ -4,12 +4,13 @@
 // TODO : clean up vectors
 void Control::configure(Commands *commands) {
     commands->visionWantedState = commands->MONOCULAR;
-    commands->displayWantedStates = std::vector<Commands::DisplayState>{commands->MESH, commands->CAMERA_POS, commands->DISPLAY_IMG};
+    commands->displayWantedStates = std::vector<Commands::DisplayState>{
+        commands->MESH, commands->CAMERA_POS, commands->DISPLAY_IMG};
     commands->uploadWantedStates =
         std::vector<std::pair<Commands::UploadState, long long>>{
-            //std::make_pair(Commands::UploadState::PH, 1000 * 15),
-            //std::make_pair(Commands::UploadState::LIGHT_INTENSITY,
-            //100 * 15)
+            // std::make_pair(Commands::UploadState::PH, 1000 * 15),
+            // std::make_pair(Commands::UploadState::LIGHT_INTENSITY,
+            // 100 * 15)
         };
 }
 
