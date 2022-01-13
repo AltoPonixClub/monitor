2165424dad40a57493f891ab7fd2cace3bf354ed
Aditya Oberai <aoberai05@gmail.com>
2022-01-12 16:52:26 -0800

    Linting action (#23)
    linter with github actions

--- unit/src/main/cpp/robot/control.cpp
+++ unit/src/main/cpp/robot/control.cpp
@@ -8,9 +8,9 @@ void Control::configure(Commands *commands) {
         commands->MESH, commands->CAMERA_POS, commands->DISPLAY_IMG};
     commands->uploadWantedStates =
         std::vector<std::pair<Commands::UploadState, long long>>{
-            //            std::make_pair(Commands::UploadState::PH, 1000 * 15),
-            //            std::make_pair(Commands::UploadState::LIGHT_INTENSITY,
-            //            100 * 15)
+            // std::make_pair(Commands::UploadState::PH, 1000 * 15),
+            // std::make_pair(Commands::UploadState::LIGHT_INTENSITY,
+            // 100 * 15)
         };
 }
 
