2d205a4fb91be44a8b4f9de66d94a83706db1fde
Maxwell938 <hellomynameisjeff680@gmail.com>
2022-01-14 00:37:29 -0800

    led blinker works with temporary timer, needs to get unix timer working

--- unit/src/main/cpp/robot.cpp
+++ unit/src/main/cpp/robot.cpp
@@ -31,8 +31,8 @@ int main(int argc, char **argv) {
         Display::instance(state, commands, outputs),
         BlinkingLights::instance(
             state, commands,
-            outputs) // Uploader::instance(state, commands, outputs)
-    };
+            outputs)//,  Uploader::instance(state, commands, outputs)
+                          };
     spdlog::info("Finished initialization");
 
     while (true) {
