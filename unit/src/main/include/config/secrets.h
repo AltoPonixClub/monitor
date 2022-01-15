b4496c3903804273311417d7b08a450405157d38
tillvit <lucas.j.tan@gmail.com>
2022-01-15 15:05:13 -0800

    Add secrets file

---
+++ unit/src/main/include/config/secrets.h
@@ -0,0 +1,16 @@
+#ifndef MONITOR_SECRETS_H
+#define MONITOR_SECRETS_H
+
+#include <string>
+
+class Secrets {
+  public:
+    static void configure();
+    class Uploader {
+      public:
+        static inline const std::string kMonitorPassword =
+            "";
+    };
+};
+
+#endif // MONITOR_SECRETS_H
