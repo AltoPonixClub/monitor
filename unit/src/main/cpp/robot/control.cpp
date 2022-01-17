e5e6a5fbf85adabc6f300ee556590de6aa0978ec
aoberai <aoberai05@gmail.com>
2022-01-16 22:42:04 -0800

    cleaned up

--- unit/src/main/cpp/robot/control.cpp
+++ unit/src/main/cpp/robot/control.cpp
@@ -7,7 +7,8 @@ void Control::configure(Commands *commands) {
     // TODO: wtaf (what the actual fuck) Doesnt work if I dont enable camera
     // pose and display img; cannot just enable mesh
     commands->displayWantedStates = std::vector<Commands::DisplayState>{
-        Commands::DisplayState::MESH, Commands::DisplayState::CAMERA_POS, Commands::DisplayState::DISPLAY_IMG};
+        Commands::DisplayState::MESH, Commands::DisplayState::CAMERA_POS,
+        Commands::DisplayState::DISPLAY_IMG};
     //    commands->displayWantedStates =
     //        std::vector<Commands::DisplayState>{commands->MESH};
     commands->uploadWantedStates =
